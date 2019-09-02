2567ae18258fb384e3645f0202a5265ccfe0fcff
fedechkin_alexey <aleksey-fedechkin@rambler.ru>
2019-09-02 11:02:50 +0700

    add Chat struct

--- src/core/types.rs
+++ src/core/types.rs
@@ -9,3 +9,20 @@ pub struct User {
     username: Option<String>,
     language_code: Option<String>,
 }
+
+#[derive(Debug, Deserialize)]
+pub struct Chat {
+    id: i64,
+    chat_type: String,
+    title: Option<String>,
+    username:Option<String>,
+    first_name: Option<String>,
+    last_name: Option<String>,
+    photo: Option<ChatPhoto>,
+    description: Option<String>,
+    invite_link: Option<String>,
+    pinned_message: Option<Message>,
+    permissions: Option<ChatPermissions>,
+    sticker_set_name: Option<String>,
+    can_set_sticker_set: Option<Bool>,
+}
