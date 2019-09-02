1087b0aa2b299d2bb543911f334ce8e18ad07e4d
RustemB <bakirov.com@ya.ru>
2019-09-03 01:22:16 +0500

    New struct: StickerSet

---
+++ src/core/types/sticker_set.rs
@@ -0,0 +1,12 @@
+use serde::Deserialize;
+
+use crate::core::types::{Sticker};
+
+#[derive(Debug, Deserialize, Hash, PartialEq, Eq)]
+pub struct Sticker {
+    pub name: String,
+    pub title: String,
+    pub is_animated: bool,
+    pub contains_masks: bool,
+    pub stickers: Vec<Sticker>,
+}
