1891976650bee19811e162b07e61aa4c3e0f9cce
P0lunin <polunindima2002@gmail.com>
2019-09-03 10:55:29 +0300

    add struct

---
+++ src/core/types/chosen_inline_result.rs
@@ -0,0 +1,11 @@
+use serde::Deserialize;
+use crate::core::types::user::User;
+
+#[derive(Debug, Deserealize)]
+pub struct ChosenInlineResult {
+    pub result_id: String,
+    pub from: User,
+    pub location: Option<Location>,
+    pub inline_message_id: Option<String>,
+    pub query: String,
+}
