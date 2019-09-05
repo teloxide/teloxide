4d241131fc88c962cf7f5abf77b53c77b6d238c7
Waffle <wafflewafflerov@gmail.com>
2019-09-05 19:12:03 +0300

    Make `ResponseParameters` more rust-friendly

--- src/core/types/response_parameter.rs
+++ src/core/types/response_parameter.rs
@@ -1,5 +1,30 @@
 #[derive(Debug, Deserialize, Hash, PartialEq, Eq, Clone)]
-pub struct ResponseParameters {
-    pub migrate_to_chat_id: Option<i64>,
-    pub retry_after: Option<i64>,
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum ResponseParameters {
+    MigrateToChatId(i64),
+    RetryAfter(i64),
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn migrate_to_chat_id_deserialization() {
+        let expected_struct = ResponseParameters::MigrateToChatId(123456);
+        let actual_json: ResponseParameters = serde_json::from_str(
+            r#"{"migrate_to_chat_id":123456}"#
+        ).unwrap();
+
+        assert_eq!(expected_json, actual_json);
+    }
+
+    #[test]
+    fn retry_after_deserialization() {
+        let expected_struct = ResponseParameters::RetryAfter(123456);
+        let actual_json: ResponseParameters = serde_json::from_str(
+            r#"{"retry_after":123456}"#
+        ).unwrap();
+
+        assert_eq!(expected_json, actual_json);
+    }
 }
