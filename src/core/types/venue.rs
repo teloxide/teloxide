26ba1919be12eb6a0df75607315c6332b27fd848
Waffle <wafflewafflerov@gmail.com>
2019-09-07 22:17:14 +0300

    Add docs for `core::types{Venue, Video}`

--- src/core/types/venue.rs
+++ src/core/types/venue.rs
@@ -1,12 +1,19 @@
 use crate::core::types::Location;
 
+
+/// This object represents a venue.
 #[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
 pub struct Venue {
+    /// Venue location
     pub location: Location,
+    /// Name of the venue
     pub title: String,
+    /// Address of the venue
     pub address: String,
+    /// Foursquare identifier of the venue
     #[serde(skip_serializing_if = "Option::is_none")]
     pub foursquare_id: Option<String>,
+    /// Foursquare type of the venue. (For example, “arts_entertainment/default”, “arts_entertainment/aquarium” or “food/icecream”.)
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub foursquare_type: Option<String>,
+    pub foursquare_type: Option<String>, // TODO: is this enum?...
 }
