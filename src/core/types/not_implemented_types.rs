cc19034bcaf3464823cdb77cb99631a136c75b3a
Mr-Andersen <andrew-anderson.neo@yandex.ru>
2019-09-12 16:04:13 +0300

    Dropped implemented types from not_implemented_types

--- src/core/types/not_implemented_types.rs
+++ src/core/types/not_implemented_types.rs
@@ -1,35 +1,8 @@
 #[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
-pub struct Location;
-
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
-pub struct InlineKeyboardMarkup;
-
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
 pub struct PassportData;
 
 #[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
-pub struct Poll;
-
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
 pub struct Animation;
-
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
-pub struct Game;
-
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
-pub struct Contact;
-
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
-pub struct VideoNote;
-
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
-pub struct Venue;
-
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
-pub struct Voice;
-
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
-pub struct MaskPosition;
 
 #[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
 pub struct ChatMemberStatus;
