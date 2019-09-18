553ffd928be6983768e1a9ae4893fe6df60e6787
Waffle <wafflewafflerov@gmail.com>
2019-09-19 01:37:45 +0300

    Fix some "unused imports" warnings

--- src/core/types/inline_query_result_photo.rs
+++ src/core/types/inline_query_result_photo.rs
@@ -2,6 +2,9 @@ use crate::core::types::{
     InlineKeyboardMarkup, InputMessageContent, ParseMode,
 };
 
+/// Represents a link to a photo. By default, this photo will be sent by the
+/// user with optional caption. Alternatively, you can use input_message_content
+/// to send a message with the specified content instead of the photo.
 #[derive(Debug, Serialize, PartialEq, Clone)]
 pub struct InlineQueryResultPhoto {
     pub id: String,
