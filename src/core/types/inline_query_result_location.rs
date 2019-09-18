f96f9a3a2b543bf364975b85e42f76e0c125696d
Waffle <wafflewafflerov@gmail.com>
2019-09-19 00:44:23 +0300

    Fix `InlineQueryResultLocation` (Add `Option<>` to optional fields)

--- src/core/types/inline_query_result_location.rs
+++ src/core/types/inline_query_result_location.rs
@@ -6,11 +6,11 @@ pub struct InlineQueryResultLocation {
     pub latitude: f64,
     pub longitude: f64,
     pub title: String,
-    pub live_period: i32,
-    pub reply_markup: InlineKeyboardMarkup,
-    pub input_message_content: InputMessageContent,
-    pub thumb_url: String,
-    pub thumb_width: i32,
-    pub thumb_height: i32,
+    pub live_period: Option<i32>,
+    pub reply_markup: Option<InlineKeyboardMarkup>,
+    pub input_message_content: Option<InputMessageContent>,
+    pub thumb_url: Option<String>,
+    pub thumb_width: Option<i32>,
+    pub thumb_height: Option<i32>,
 
 }
