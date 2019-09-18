ed09426c1518f818188a0f99af7edc476edd4b8c
Temirkhan Myrzamadi <gymmasssorla@gmail.com>
2019-09-18 23:52:54 +0600

    Add InlineQueryResultCachedVideo

---
+++ src/core/types/inline_query_result_cached_video.rs
@@ -0,0 +1,16 @@
+use super::inline_keyboard_markup::InlineKeyboardMarkup;
+use super::input_message_content::InputMessageContent;
+
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+pub struct InlineQueryResultCachedVideo {
+    #[serde(rename = "type")]
+    pub query_type: String,
+    pub id: String,
+    pub video_file_id: String,
+    pub title: String,
+    pub description: Option<String>,
+    pub caption: Option<String>,
+    pub parse_mode: Option<String>,
+    pub reply_markup: Option<InlineKeyboardMarkup>,
+    pub input_message_content: Option<InputMessageContent>,
+}
