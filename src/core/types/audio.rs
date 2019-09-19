7b638eb2c0a73b02b99d4ed04cb05a0e2cfe90a9
P0lunin <polunindima2002@gmail.com>
2019-09-19 19:38:07 +0300

    add tests

--- src/core/types/audio.rs
+++ src/core/types/audio.rs
@@ -1,17 +1,51 @@
 use crate::core::types::PhotoSize;
 
-#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize, Clone)]
+#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Clone)]
 pub struct Audio {
     pub file_id: String,
     pub duration: u32,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub performer: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub title: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub mime_type: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub file_size: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub thumb: Option<PhotoSize>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn deserialize() {
+        let json = r#"{
+            "file_id":"id",
+            "duration":60,
+            "performer":"Performer",
+            "title":"Title",
+            "mime_type":"MimeType",
+            "file_size":123456,
+            "thumb":{
+                "file_id":"id",
+                "width":320,
+                "height":320,
+                "file_size":3452
+            }
+        }"#;
+        let expected = Audio {
+            file_id: "id".to_string(),
+            duration: 60,
+            performer: Some("Performer".to_string()),
+            title: Some("Title".to_string()),
+            mime_type: Some("MimeType".to_string()),
+            file_size: Some(123456),
+            thumb: Some(PhotoSize {
+                file_id: "id".to_string(),
+                width: 320,
+                height: 320,
+                file_size: Some(3452)
+            })
+        };
+        let actual = serde_json::from_str::<Audio>(&json).unwrap();
+        assert_eq!(actual, expected)
+    }
 }
--- src/core/types/user.rs
+++ src/core/types/user.rs
@@ -7,3 +7,30 @@ pub struct User {
     pub username: Option<String>,
     pub language_code: Option<String>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn deserialize() {
+        let json = r#"{
+            "id":12345,
+            "is_bot":false,
+            "first_name":"firstName",
+            "last_name":"lastName",
+            "username":"Username",
+            "language_code":"languageCode"
+        }"#;
+        let expected = User {
+            id: 12345,
+            is_bot: false,
+            first_name: "firstName".to_string(),
+            last_name: Some("lastName".to_string()),
+            username: Some("Username".to_string()),
+            language_code: Some("languageCode".to_string())
+        };
+        let actual = serde_json::from_str::<User>(&json).unwrap();
+        assert_eq!(actual, expected)
+    }
+}
