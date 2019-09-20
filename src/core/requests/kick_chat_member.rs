f4b9e9344726e6e6304af71708b982f12b0108be
nextel <rm.nextel@gmail.com>
2019-09-20 17:42:24 +0300

    cargo fmt for requests/*

--- src/core/requests/kick_chat_member.rs
+++ src/core/requests/kick_chat_member.rs
@@ -1,6 +1,10 @@
 use crate::core::requests::RequestContext;
 //TODO:: need implementation
-
+/// Use this method to kick a user from a group, a supergroup or a channel. In
+/// the case of supergroups and channels, the user will not be able to return to
+/// the group on their own using invite links, etc., unless unbanned first. The
+/// bot must be an administrator in the chat for this to work and must have the
+/// appropriate admin rights. Returns True on success.
 #[derive(Debug, Clone, Serialize)]
 struct KickChatMember<'a> {
     #[serde(skip_serializing)]
