23f2b9494d58ad963b3335427bbfe4e086ff6cfd
nextel <rm.nextel@gmail.com>
2019-09-21 20:08:06 +0300

    add action template

--- src/requests/kick_chat_member.rs
+++ src/requests/kick_chat_member.rs
@@ -9,4 +9,8 @@ use crate::requests::RequestContext;
 pub struct KickChatMember<'a> {
     #[serde(skip_serializing)]
     ctx: RequestContext<'a>,
+
+    chat_id 	Integer or String 	Yes 	Unique identifier for the target group or username of the target supergroup or channel (in the format @channelusername)
+    user_id 	Integer 	Yes 	Unique identifier of the target user
+    until_date 	Integer 	Optional 	Date when the user will be unbanned, unix time. If user is banned for more than 366 days or less than 30 seconds from the current time they are considered to be banned forever
 }
