e3d31ccf977c20e34e3308e66cf4312b31cda57f
nextel <rm.nextel@gmail.com>
2019-10-08 18:37:12 +0300

    cargo fmt to add document action

--- src/requests/send_document.rs
+++ src/requests/send_document.rs
@@ -5,7 +5,6 @@ use async_trait::async_trait;
 
 // TODO: add method to bot/api
 
-
 ///Use this method to send general files. On success, the sent Message is
 /// returned. Bots can currently send files of any type of up to 50 MB in size,
 /// this limit may be changed in the future.
