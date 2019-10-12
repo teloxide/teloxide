7a4442a0594d694e6ba2a34ecbb8d215d0504947
Nextel <rm.nextel@gmail.com>
2019-10-12 22:31:27 +0300

    cargo fmt to sent animation action

--- src/requests/send_animation.rs
+++ src/requests/send_animation.rs
@@ -47,7 +47,6 @@ pub struct SendAnimation<'a> {
     pub reply_markup: Option<ReplyMarkup>,
 }
 
-
 #[async_trait]
 impl<'a> Request for SendAnimation<'a> {
     type ReturnValue = Message;
@@ -65,10 +64,9 @@ impl SendAnimation<'_> {
             "sendAnimation",
             &self,
         )
-            .await
+        .await
     }
 }
-
 
 impl<'a> SendAnimation<'a> {
     pub(crate) fn new(
@@ -93,75 +91,73 @@ impl<'a> SendAnimation<'a> {
     }
 
     pub fn chat_id<T>(mut self, chat_id: T) -> Self
-        where
-            T: Into<ChatId>,
+    where
+        T: Into<ChatId>,
     {
         self.chat_id = chat_id.into();
         self
     }
 
-
     pub fn duration<T>(mut self, duration: T) -> Self
-        where
-            T: Into<u64>,
+    where
+        T: Into<u64>,
     {
         self.duration = Some(duration.into());
         self
     }
 
-
     pub fn width<T>(mut self, width: T) -> Self
-        where
-            T: Into<i32>,
+    where
+        T: Into<i32>,
     {
         self.width = Some(width.into());
         self
     }
     pub fn height<T>(mut self, height: T) -> Self
-        where
-            T: Into<i32>,
+    where
+        T: Into<i32>,
     {
         self.height = Some(height.into());
         self
     }
     pub fn thumb<T>(mut self, thumb: T) -> Self
-        where
-            T: Into<String>,
+    where
+        T: Into<String>,
     {
         self.thumb = Some(thumb.into());
         self
     }
     pub fn caption<T>(mut self, caption: T) -> Self
-        where
-            T: Into<String>,
+    where
+        T: Into<String>,
     {
         self.caption = Some(caption.into());
         self
     }
     pub fn parse_mode<T>(mut self, parse_mode: T) -> Self
-        where
-            T: Into<ParseMode>,
+    where
+        T: Into<ParseMode>,
     {
         self.parse_mode = Some(parse_mode.into());
         self
     }
     pub fn disable_notification<T>(mut self, disable_notification: T) -> Self
-        where
-            T: Into<bool>,
+    where
+        T: Into<bool>,
     {
         self.disable_notification = Some(disable_notification.into());
         self
     }
     pub fn reply_to_message_id<T>(mut self, reply_to_message_id: T) -> Self
-        where
-            T: Into<i32>,
+    where
+        T: Into<i32>,
     {
         self.reply_to_message_id = Some(reply_to_message_id.into());
         self
     }
     pub fn reply_markup<T>(mut self, reply_markup: T) -> Self
-        where
-            T: Into<ReplyMarkup>,
+    where
+        T: Into<ReplyMarkup>,
     {
         self.reply_markup = Some(reply_markup.into());
         self
