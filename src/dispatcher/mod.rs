175e7572b224802500d3d907d9b04e7a5f42b460
Waffle <wafflewafflerov@gmail.com>
2019-10-16 14:47:09 +0300

    Add dispatcher trait

--- src/dispatcher/mod.rs
+++ src/dispatcher/mod.rs
@@ -7,3 +7,10 @@ pub mod updater;
 
 pub use filter::Filter;
 pub use handler::Handler;
+
+use async_trait::async_trait;
+
+#[async_trait(?Send)]
+pub trait Dispatcher<'a, U> {
+    async fn dispatch(&'a mut self, updater: U);
+}
