66147232e6411fffe73f2c3b5c980403c23cc594
RustemB <bakirov.com@ya.ru>
2019-10-20 12:30:07 +0500

    levaeChat

---
+++ src/requests/leave_chat.rs
@@ -0,0 +1,50 @@
+use async_trait::async_trait;
+
+use crate::{
+    bot::Bot,
+    network,
+    requests::{Request, ResponseResult},
+    types::ChatId,
+};
+
+/// Use this method for your bot to leave a group, supergroup or channel.
+/// Returns True on success.
+#[derive(Debug, Clone, Serialize)]
+pub struct LeaveChat<'a> {
+    #[serde(skip_serializing)]
+    bot: &'a Bot,
+    /// Unique identifier for the target chat or username
+    /// of the target supergroup or channel (in the format @channelusername)
+    chat_id: ChatId,
+}
+
+#[async_trait]
+impl Request for LeaveChat<'_> {
+    type Output = bool;
+
+    async fn send_boxed(self) -> ResponseResult<Self::Output> {
+        self.send().await
+    }
+}
+
+impl LeaveChat<'_> {
+    pub async fn send(self) -> ResponseResult<bool> {
+        network::request_json(
+            self.bot.client(),
+            self.bot.token(),
+            "leaveChat",
+            &self,
+        )
+        .await
+    }
+}
+
+impl<'a> LeaveChat<'a> {
+    pub fn chat_id<C>(mut self, value: C) -> Self
+    where
+        C: Into<ChatId>,
+    {
+        self.chat_id = value.into();
+        self
+    }
+}
