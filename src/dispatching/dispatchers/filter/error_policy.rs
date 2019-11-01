1d9c42c309097a857a39afca2ff2fb43c310859a
Temirkhan Myrzamadi <gymmasssorla@gmail.com>
2019-11-01 21:51:09 +0600

    Fix

--- src/dispatching/dispatchers/filter/error_policy.rs
+++ src/dispatching/dispatchers/filter/error_policy.rs
@@ -4,10 +4,8 @@ use async_trait::async_trait;
 
 /// Implementors of this trait are treated as error-handlers.
 #[async_trait]
-pub trait ErrorPolicy {
-    type Error;
-
-    async fn handle_error(&mut self, error: Self::Error);
+pub trait ErrorPolicy<E> {
+    async fn handle_error(&mut self, error: E);
 }
 
 /// A convenient structure with an error-handling closure. Implements
@@ -15,14 +13,12 @@ pub trait ErrorPolicy {
 pub struct FnErrorPolicy<F>(pub F);
 
 #[async_trait]
-impl<E, F, Fut> ErrorPolicy<Error = E> for FnErrorPolicy<F>
+impl<E, F, Fut> ErrorPolicy<E> for FnErrorPolicy<F>
 where
     F: FnMut(E) -> Fut + Send,
     Fut: Future<Output = ()>,
     E: Send,
 {
-    type Error = E;
-
     async fn handle_error(&mut self, error: E) {
         self.0(error);
     }
