08ebb3cc01e368829b69bfcd5f16df2b37a4bd5c
Waffle <wafflewafflerov@gmail.com>
2019-11-02 15:55:54 +0300

    Fix `ErrorPolicy` by implementing trait on all `Fn() -> Fut` by-hands (and some related small fixes)

--- src/dispatching/dispatchers/filter/error_policy.rs
+++ src/dispatching/dispatchers/filter/error_policy.rs
@@ -1,21 +1,28 @@
 use std::future::Future;
 
 use async_trait::async_trait;
+use std::pin::Pin;
 
 /// Implementors of this trait are treated as error-handlers.
 #[async_trait]
 pub trait ErrorPolicy<E> {
-    async fn handle_error(&mut self, error: E);
+    async fn handle_error(&self, error: E)
+    where
+        E: 'async_trait;
 }
 
-#[async_trait]
 impl<E, F, Fut> ErrorPolicy<E> for F
-where
-    F: FnMut(E) -> Fut + Send,
-    Fut: Future<Output = ()> + Send,
-    E: Send,
+    where
+        F: Fn(E) -> Fut + Sync,
+        Fut: Future<Output = ()> + Send,
+        E: Send,
 {
-    async fn handle_error(&mut self, error: E) {
-        self(error).await;
+    fn handle_error<'s, 'async_trait>(&'s self, error: E) -> Pin<Box<dyn Future<Output = ()> + Send + 'async_trait>>
+    where
+        's: 'async_trait,
+        Self: 'async_trait,
+        E: 'async_trait
+    {
+        Box::pin(async move { self(error).await })
     }
 }
