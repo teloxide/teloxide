a20b3f132aeb53c14b5bb55e0ec6288062c63bd9
Waffle <wafflewafflerov@gmail.com>
2019-11-07 19:04:57 +0300

    Derive default for `GetMe`

--- src/requests/payloads/get_me.rs
+++ src/requests/payloads/get_me.rs
@@ -3,7 +3,7 @@ use crate::{
     types::User,
 };
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, Default, Deserialize, Serialize)]
 /// A filter method for testing your bot's auth token. Requires no parameters.
 /// Returns basic information about the bot in form of a [`User`] object.
 ///
