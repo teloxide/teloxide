807e3dc8515390c7ff2315d968c6459e04019559
Temirkhan Myrzamadi <gymmasssorla@gmail.com>
2019-12-13 10:20:49 +0600

    Insert the icon into docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![allow(clippy::unit_arg)] // TODO
 #![allow(clippy::ptr_arg)] // TODO
+#![doc(
+    html_logo_url = "https://github.com/teloxide/teloxide/raw/dev/ICON.png",
+    html_favicon_url = "https://github.com/teloxide/teloxide/raw/dev/ICON.png"
+)]
 
 #[macro_use]
 extern crate derive_more;
