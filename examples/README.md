45a032116d5967faf5e2c22f45731186507c74b8
Sergey Levitin <selevit@gmail.com>
2020-01-11 16:02:49 +0300

    Add ping_bot example

---
+++ examples/README.md
@@ -0,0 +1,9 @@
+# Examples of how to use Teloxide.
+
+All examples can be executed with:
+
+```
+cargo run --example $name
+```
+
+If you've got an example you'd like to see here, please feel free to open an issue. Otherwise if you've got an example you'd like to add, please feel free to make a PR!
---
+++ examples/ping_bot.rs
@@ -0,0 +1,43 @@
+use futures::stream::StreamExt;
+use teloxide::{
+    dispatching::{
+        private::Dispatcher, update_listeners::polling_default, SessionState,
+    },
+    requests::Request,
+    types::{Message, Update, UpdateKind},
+    Bot,
+};
+
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    let bot = &Bot::new("1061598315:AAErEDodTsrqD3UxA_EvFyEfXbKA6DT25G0");
+    let mut updater = Box::pin(polling_default(bot));
+    let handler = |s, upd: Update| async move {
+        match upd.kind {
+            UpdateKind::Message(m) => {
+                if m.text() == "ping" {
+                    let msg = bot.send_message(m.chat.id, "pong");
+                }
+                msg.send().await.unwrap();
+            }
+            _ => {}
+        }
+        SessionState::Continue(s)
+    };
+    let mut dp = Dispatcher::<'_, (), _>::new(handler);
+    info!("Starting the message handler.");
+    loop {
+        let u = updater.next().await.unwrap();
+        match u {
+            Err(e) => error!("{}", e),
+            Ok(u) => {
+                let _ = dp.dispatch(u).await;
+            }
+        }
+    }
+}
