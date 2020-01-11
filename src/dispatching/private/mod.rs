5fd6ae3630151ea6f100e63406152e0e24637a4a
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-01-11 21:04:57 +0600

    Fix the docs

--- src/dispatching/private/mod.rs
+++ src/dispatching/private/mod.rs
@@ -4,23 +4,31 @@
 //!
 //!  1. Your session type `Session`, which designates a dialogue state at the
 //! current moment.
-//!  2. A storage that encapsulates all the sessions.
+//!  2. [`Storage`] that encapsulates all the sessions.
 //!  3. Your handler of type `H: async Fn(Session, Update) ->
 //! HandleResult<Session>` that receives an update and turns your session into
 //! the next state.
-//!  4. The dispatcher, which encapsulates your handler and [`Storage`], and has
-//! the `dispatch(Update) -> DispatchResult` function.
+//!  4. [`Dispatcher`], which encapsulates your handler and [`Storage`], and has
+//! the [`dispatch(Update) -> DispatchResult`] function.
 //!
-//! Every time you call `.dispatch(update)` on your updater, the following steps
-//! are executed:
+//! Every time you call [`.dispatch(update)`] on your dispatcher, the following
+//! steps are executed:
 //!
 //!  1. If a supplied update is not from a 1-to-1 chat, return
-//! `DispatchResult::Unhandled`.
+//! [`DispatchResult::Unhandled`].
 //!  2. If a storage doesn't contain a session from this chat, supply
 //! `Session::default()` into you handler, otherwise, supply the previous
 //! session.
-//!  3. If a handler has returned `HandleResult::Terminate`, remove the sesion
+//!  3. If a handler has returned [`SessionState::Terminate`], remove the sesion
 //! from a storage, otherwise force the storage to update the session.
+//!
+//! [`Storage`]: crate::dispatching::private::Storage
+//! [`Dispatcher`]: crate::dispatching::private::Dispatcher
+//! [`dispatch(Update) -> DispatchResult`]:
+//! crate::dispatching::private::Dispatcher::dispatch
+//! [`.dispatch(update)`]: crate::dispatching::private::Dispatcher::dispatch
+//! [`DispatchResult::Unhandled`]: crate::dispatching::DispatchResult::Unhandled
+//! [`SessionState::Terminate`]: crate::dispatching::SessionState::Terminate
 
 // TODO: examples
 
