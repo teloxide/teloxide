4db07fc189e50b4e54707d0ee0d4292c405be65e
p0lunin <dmytro.polunin@gmail.com>
2020-01-21 10:41:10 +0200

    delete filters

--- src/dispatching/mod.rs
+++ src/dispatching/mod.rs
@@ -2,12 +2,10 @@
 
 //mod dispatchers;
 pub mod error_handlers;
-pub mod filters;
 mod handler;
 pub mod updaters;
 
 //pub use dispatchers::filter::FilterDispatcher;
 pub use error_handlers::ErrorHandler;
-pub use filters::Filter;
 pub use handler::Handler;
 pub use updaters::Updater;
