9c020a23cb021018426e391924fdb73483a7104e
p0lunin <dmytro.polunin@gmail.com>
2020-01-21 10:42:43 +0200

    remove feature regex-filter

--- src/dispatching/mod.rs
+++ src/dispatching/mod.rs
@@ -1,11 +1,11 @@
 //! Update dispatching.
 
-//mod dispatchers;
+mod dispatchers;
 pub mod error_handlers;
 mod handler;
 pub mod updaters;
 
-//pub use dispatchers::filter::FilterDispatcher;
+pub use dispatchers::filter::FilterDispatcher;
 pub use error_handlers::ErrorHandler;
 pub use handler::Handler;
 pub use updaters::Updater;
