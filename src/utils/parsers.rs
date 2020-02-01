8b9058192754433abf469961410537d177e5e449
p0lunin <dmytro.polunin@gmail.com>
2020-02-01 21:37:36 +0200

    added documentation

--- src/utils/parsers.rs
+++ src/utils/parsers.rs
@@ -1,3 +1,41 @@
+//! Command parsers
+//!
+//! Use can create `enum` which contains commands you need to parse, or use functions
+//! which split input text into string command.
+//!
+//! Example of enum:
+//! ```
+//! use teloxide::utils::BotCommand;
+//!
+//! #[derive(BotCommand, PartialEq, Debug)]
+//! #[command(rename = "lowercase")]
+//! enum AdminCommand {
+//!     Kick,
+//!     Ban,
+//! }
+//!
+//! let (command, args) = AdminCommand::parse("/ban 3 hours").unwrap();
+//! assert_eq!(command, AdminCommand::Ban);
+//! assert_eq!(args, vec!["3", "hours"]);
+//! ```
+//!
+//! Also you can use functions which split text into command and args (separator is space):
+//! ```
+//! let (command, args) = teloxide::utils::parse_command("/ban 3 hours").unwrap();
+//! assert_eq!(command, "/ban");
+//! assert_eq!(args, vec!["3", "hours"]);
+//! ```
+//!
+//! Or similar function, which parse text with prefix removal:
+//! ```
+//! let text = "!ban 3 hours";
+//! let (command, args) =
+//!     teloxide::utils::parse_command_with_prefix("!", text).unwrap();
+//! assert_eq!(command, "ban");
+//! assert_eq!(args, vec!["3", "hours"]);
+//! ```
+
+
 pub use teloxide_macros::BotCommand;
 
 /// An enumeration of bot's commands.
@@ -17,6 +55,27 @@ pub use teloxide_macros::BotCommand;
 /// assert_eq!(command, AdminCommand::Ban);
 /// assert_eq!(args, vec!["5", "h"]);
 /// ```
+///
+/// Enum attributes:
+/// 1. `#[command(rename = "rule")]`
+/// Use this attribute when you need to rename all commands by rule. Allowed rules is
+/// `["lowercase"]`. If you will not use this attribute, commands will parse by them original
+/// names.
+/// 2. `#[command(prefix = "prefix")]`
+/// Use this attribute when you need to change prefix for all command. Defaule prefix is `/`.
+/// 3. `#[command(description = "description")]`
+/// Use this attribute when you need to add description of commands before all commands.
+///
+/// Variant attributes:
+/// 1. `#[command(rename = "rule")]`
+/// Use this attribute when you need to renameone command by rule. Allowed rules is
+/// `["lowercase", "%some_name%"]`, where `%some_name%` is any string.
+/// 2. `#[command(prefix = "prefix")]`
+/// Use this attribute when you need to change prefix for one command. Default prefix is `/`.
+/// 3. `#[command(description = "description")]`
+/// Use this attribute when you need to add description of one command.
+///
+/// All variant attributes overlap the enum attributes.
 pub trait BotCommand: Sized {
     fn try_from(s: &str) -> Option<Self>;
     fn descriptions() -> String;
