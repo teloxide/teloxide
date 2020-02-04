32f44f83abc74f6efb8f18e0cc537bee8aaa8f27
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-02-04 21:39:42 +0600

    A quick fix

--- src/dispatching/dialogue/dialogue.rs
+++ src/dispatching/dialogue/dialogue.rs
@@ -1,24 +1,4 @@
 /// A type, encapsulating a dialogue state and arbitrary data.
-///
-/// ## Example
-/// ```
-/// use teloxide::dispatching::dialogue::Dialogue;
-///
-/// enum MyState {
-///     FullName,
-///     Age,
-///     FavouriteMusic,
-/// }
-///
-/// #[derive(Default)]
-/// struct User {
-///     full_name: Option<String>,
-///     age: Option<u8>,
-///     favourite_music: Option<String>,
-/// }
-///
-/// let _dialogue = Dialogue::new(MyState::FullName, User::default());
-/// ```
 #[derive(Default, Debug, Copy, Clone, Eq, Hash, PartialEq)]
 pub struct Dialogue<State, T> {
     pub state: State,
