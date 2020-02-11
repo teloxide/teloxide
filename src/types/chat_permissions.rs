98345b4d45f70b8bb98fc377bbc648c7a1f07ac3
p0lunin <dmytro.polunin@gmail.com>
2020-02-11 21:35:06 +0200

    added Default to ChatPermissions

--- src/types/chat_permissions.rs
+++ src/types/chat_permissions.rs
@@ -39,3 +39,18 @@ pub struct ChatPermissions {
     /// supergroups.
     pub can_pin_messages: Option<bool>,
 }
+
+impl Default for ChatPermissions {
+    fn default() -> Self {
+        Self {
+            can_send_messages: None,
+            can_send_media_messages: None,
+            can_send_polls: None,
+            can_send_other_messages: None,
+            can_add_web_page_previews: None,
+            can_change_info: None,
+            can_invite_users: None,
+            can_pin_messages: None
+        }
+    }
+}
