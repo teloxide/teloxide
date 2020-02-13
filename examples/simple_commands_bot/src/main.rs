2147ba204809e0e5f909b76ce25d52f5d81e665f
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-02-14 03:23:41 +0600

    Add examples/simple_commands_bot

---
+++ examples/simple_commands_bot/src/main.rs
@@ -0,0 +1,54 @@
+use teloxide::{prelude::*, utils::command::BotCommand};
+
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "be a cat.")]
+    Meow,
+    #[command(description = "be a dog.")]
+    Woof,
+    #[command(description = "be a cow.")]
+    Moo,
+}
+
+async fn handle_command(
+    ctx: DispatcherHandlerCtx<Message>,
+) -> Result<(), RequestError> {
+    let text = match ctx.update.text() {
+        Some(text) => text,
+        None => return Ok(()),
+    };
+
+    let command = match Command::parse(text) {
+        Some((command, _)) => command,
+        None => return Ok(()),
+    };
+
+    match command {
+        Command::Help => ctx.answer(Command::descriptions()).send().await?,
+        Command::Meow => ctx.answer("I am a cat! Meow!").send().await?,
+        Command::Woof => ctx.answer("I am a dog! Woof!").send().await?,
+        Command::Moo => ctx.answer("I am a cow! Moo!").send().await?,
+    };
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    run().await;
+}
+
+async fn run() {
+    teloxide::enable_logging!();
+    log::info!("Starting ping_pong_bot!");
+
+    let bot = Bot::from_env();
+
+    Dispatcher::<RequestError>::new(bot)
+        .message_handler(&handle_command)
+        .dispatch()
+        .await;
+}
