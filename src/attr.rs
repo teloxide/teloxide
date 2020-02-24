c6e22c3f8a7583c87ff8d74c8bf754555376fc45
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-02-24 14:48:37 +0600

    Init

---
+++ src/attr.rs
@@ -0,0 +1,64 @@
+use syn::{
+    parse::{Parse, ParseStream},
+    LitStr, Token,
+};
+
+pub enum BotCommandAttribute {
+    Prefix,
+    Description,
+    RenameRule,
+}
+
+impl Parse for BotCommandAttribute {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let name_arg: syn::Ident = input.parse()?;
+        match name_arg.to_string().as_str() {
+            "prefix" => Ok(BotCommandAttribute::Prefix),
+            "description" => Ok(BotCommandAttribute::Description),
+            "rename" => Ok(BotCommandAttribute::RenameRule),
+            _ => Err(syn::Error::new(name_arg.span(), "unexpected argument")),
+        }
+    }
+}
+
+pub struct Attr {
+    name: BotCommandAttribute,
+    value: String,
+}
+
+impl Parse for Attr {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let name = input.parse::<BotCommandAttribute>()?;
+        input.parse::<Token![=]>()?;
+        let value = input.parse::<LitStr>()?.value();
+
+        Ok(Self { name, value })
+    }
+}
+
+impl Attr {
+    pub fn name(&self) -> &BotCommandAttribute {
+        &self.name
+    }
+
+    pub fn value(&self) -> String {
+        self.value.clone()
+    }
+}
+
+pub struct VecAttrs {
+    pub data: Vec<Attr>,
+}
+
+impl Parse for VecAttrs {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let mut data = vec![];
+        while !input.is_empty() {
+            data.push(input.parse()?);
+            if !input.is_empty() {
+                input.parse::<Token![,]>()?;
+            }
+        }
+        Ok(Self { data })
+    }
+}
---
+++ src/enum_attributes.rs
@@ -0,0 +1,50 @@
+use crate::attr::{Attr, BotCommandAttribute};
+
+pub struct CommandEnum {
+    pub prefix: Option<String>,
+    pub description: Option<String>,
+    pub rename_rule: Option<String>,
+}
+
+impl CommandEnum {
+    pub fn try_from(attrs: &[Attr]) -> Result<Self, String> {
+        let attrs = parse_attrs(attrs)?;
+
+        let prefix = attrs.prefix;
+        let description = attrs.description;
+        let rename = attrs.rename;
+        if let Some(rename_rule) = &rename {
+            match rename_rule.as_str() {
+                "lowercase" => {}
+                _ => return Err("disallowed value".to_owned()),
+            }
+        }
+        Ok(Self { prefix, description, rename_rule: rename })
+    }
+}
+
+struct CommandAttrs {
+    prefix: Option<String>,
+    description: Option<String>,
+    rename: Option<String>,
+}
+
+fn parse_attrs(attrs: &[Attr]) -> Result<CommandAttrs, String> {
+    let mut prefix = None;
+    let mut description = None;
+    let mut rename_rule = None;
+
+    for attr in attrs {
+        match attr.name() {
+            BotCommandAttribute::Prefix => prefix = Some(attr.value()),
+            BotCommandAttribute::Description => {
+                description = Some(attr.value())
+            }
+            BotCommandAttribute::RenameRule => rename_rule = Some(attr.value()),
+            #[allow(unreachable_patterns)]
+            _ => return Err("unexpected attribute".to_owned()),
+        }
+    }
+
+    Ok(CommandAttrs { prefix, description, rename: rename_rule })
+}
