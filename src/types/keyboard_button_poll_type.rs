f436cc07f51967f0854df5546f29546fbea89c2c
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-07-28 01:01:43 +0600

    Add setters to KeyboardButtonPollType

--- src/types/keyboard_button_poll_type.rs
+++ src/types/keyboard_button_poll_type.rs
@@ -5,3 +5,20 @@ use serde::{Deserialize, Serialize};
 pub struct KeyboardButtonPollType {
     poll_type: String,
 }
+
+impl KeyboardButtonPollType {
+    pub fn new<S>(poll_type: S) -> Self
+    where
+        S: Into<String>,
+    {
+        Self { poll_type: poll_type.into() }
+    }
+
+    pub fn poll_type<S>(mut self, val: S) -> Self
+    where
+        S: Into<String>,
+    {
+        self.poll_type = val.into();
+        self
+    }
+}
