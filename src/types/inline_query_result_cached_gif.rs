49b622dc6c185a593af0e8bdfd1f3573373e3351
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-07-27 19:39:26 +0600

    Add setters to InlineQueryResultCachedGif

--- src/types/inline_query_result_cached_gif.rs
+++ src/types/inline_query_result_cached_gif.rs
@@ -42,3 +42,68 @@ pub struct InlineQueryResultCachedGif {
     /// Content of the message to be sent instead of the GIF animation.
     pub input_message_content: Option<InputMessageContent>,
 }
+
+impl InlineQueryResultCachedGif {
+    pub fn new<S1, S2>(id: S1, gif_file_id: S2) -> Self
+    where
+        S1: Into<String>,
+        S2: Into<String>,
+    {
+        Self {
+            id: id.into(),
+            gif_file_id: gif_file_id.into(),
+            title: None,
+            caption: None,
+            parse_mode: None,
+            reply_markup: None,
+            input_message_content: None,
+        }
+    }
+
+    pub fn id<S>(mut self, val: S) -> Self
+    where
+        S: Into<String>,
+    {
+        self.id = val.into();
+        self
+    }
+
+    pub fn gif_file_id<S>(mut self, val: S) -> Self
+    where
+        S: Into<String>,
+    {
+        self.gif_file_id = val.into();
+        self
+    }
+
+    pub fn title<S>(mut self, val: S) -> Self
+    where
+        S: Into<String>,
+    {
+        self.title = Some(val.into());
+        self
+    }
+
+    pub fn caption<S>(mut self, val: S) -> Self
+    where
+        S: Into<String>,
+    {
+        self.caption = Some(val.into());
+        self
+    }
+
+    pub fn parse_mode(mut self, val: ParseMode) -> Self {
+        self.parse_mode = Some(val);
+        self
+    }
+
+    pub fn reply_markup(mut self, val: InlineKeyboardMarkup) -> Self {
+        self.reply_markup = Some(val);
+        self
+    }
+
+    pub fn input_message_content(mut self, val: InputMessageContent) -> Self {
+        self.input_message_content = Some(val);
+        self
+    }
+}
