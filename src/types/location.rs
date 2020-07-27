1ce57eedbcbf9a794c472ee3188888b2549396bf
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-07-28 01:07:18 +0600

    Add setters to Location

--- src/types/location.rs
+++ src/types/location.rs
@@ -10,3 +10,19 @@ pub struct Location {
     /// Latitude as defined by sender.
     pub latitude: f64,
 }
+
+impl Location {
+    pub fn new(longitude: f64, latitude: f64) -> Self {
+        Self { longitude, latitude }
+    }
+
+    pub fn latitude(mut self, val: f64) -> Self {
+        self.latitude = val;
+        self
+    }
+
+    pub fn longitude(mut self, val: f64) -> Self {
+        self.longitude = val;
+        self
+    }
+}
