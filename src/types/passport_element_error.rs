670109fdcaff12319588eaedb1ff7d9846b7dc02
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-07-28 15:41:05 +0600

    Add setters to PassportElementErrorFiles

--- src/types/passport_element_error.rs
+++ src/types/passport_element_error.rs
@@ -291,6 +291,28 @@ pub struct PassportElementErrorFiles {
     pub file_hashes: Vec<String>,
 }
 
+impl PassportElementErrorFiles {
+    pub fn new<S>(r#type: PassportElementErrorFilesType, file_hash: S) -> Self
+    where
+        S: Into<String>,
+    {
+        Self { r#type, file_hash: file_hash.into() }
+    }
+
+    pub fn r#type(mut self, val: PassportElementErrorFilesType) -> Self {
+        self.r#type = val;
+        self
+    }
+
+    pub fn file_hash<S>(mut self, val: S) -> Self
+    where
+        S: Into<String>,
+    {
+        self.file_hash = val.into();
+        self
+    }
+}
+
 /// Represents an issue with one of the files that constitute the
 /// translation of a document.
 ///
