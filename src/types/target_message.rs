02400b18ab3c0a091c60909a8664d431be8b4c68
Waffle <waffle.lapkin@gmail.com>
2020-08-10 18:14:52 +0300

    remove TargetMessage as ChatOrInlineMessage reexport as it couldn't be deprecated
    Also rename `TargetMessage::{Chat => Common}` variant.
    (anyway `ChatOrInlineMessage`/`TargetMessage` refactoring is not backward compatible)

--- src/types/target_message.rs
+++ src/types/target_message.rs
@@ -6,12 +6,9 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum TargetMessage {
-    Chat { chat_id: ChatId, message_id: i32 },
+    Common { chat_id: ChatId, message_id: i32 },
     Inline { inline_message_id: String },
 }
-
-#[deprecated = "Was renamed to `TargetMessage`, please use renamed version"]
-pub use TargetMessage as ChatOrInlineMessage;
 
 impl From<String> for TargetMessage {
     fn from(inline_message_id: String) -> Self {
