b93947c0ab2da8719605456609b0f541075c27b4
Waffle <waffle.lapkin@gmail.com>
2021-01-15 22:19:42 +0300

    stop using serde internall module

--- src/types/non_telegram_types/mime.rs
+++ src/types/non_telegram_types/mime.rs
@@ -70,10 +70,7 @@ struct MimeVisitor;
 impl<'a> Visitor<'a> for MimeVisitor {
     type Value = MimeDe;
 
-    fn expecting(
-        &self,
-        formatter: &mut fmt::Formatter<'_>,
-    ) -> Result<(), serde::export::fmt::Error> {
+    fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         formatter.write_str("mime type")
     }
 
