ab6a57de3abf6955eed345672f0c6990ddd56547
p0lunin <dmytro.polunin@gmail.com>
2021-01-29 12:24:45 +0200

    docs for demux.rs module

--- src/dispatching/core/demux.rs
+++ src/dispatching/core/demux.rs
@@ -1,23 +1,52 @@
+//! The module for the `Demux` and `DemuxBuilder` structs.
+//!
+//! The `Demux` struct try to handle the input update by the vector of handlers and return the update if no
+//! handler had handle it.
+//!
+//! The `DemuxBuilder` struct is the builder for the `Demux`.
+//!
+//! Usage:
+//! ```
+//! use teloxide::dispatching::dev::*;
+//! use std::convert::Infallible;
+//! use teloxide::dispatching::updates;
+//! use teloxide::types::Update;
+//!
+//! let mut builder = DemuxBuilder::<Update, Infallible>::new();
+//! builder.add_service(updates::any());
+//! let demux = builder.build();
+//! ```
+
 use crate::dispatching::core::{HandleFuture, Handler};
 use std::sync::Arc;
 
+/// The `Demux` struct try to handle the input update by the vector of handlers and return the update if no
+/// handler had handle it.
 pub struct Demux<Upd, Err> {
     handlers: Arc<[Box<dyn Handler<Upd, Err> + Send + Sync>]>,
 }
 
+/// The builder for the [`Demux`] struct.
 pub struct DemuxBuilder<Upd, Err> {
     handlers: Vec<Box<dyn Handler<Upd, Err> + Send + Sync>>,
 }
 
 impl<Upd, Err> DemuxBuilder<Upd, Err> {
+    /// Create the builder.
     pub fn new() -> Self {
         DemuxBuilder { handlers: Vec::new() }
     }
 
+    /// Add the handler. [`Demux`] has no methods for adding handlers.
+    ///
+    /// [`Demux`]: TODO
     pub fn add_service(&mut self, service: impl Handler<Upd, Err> + Send + Sync + 'static) {
         self.handlers.push(Box::new(service) as _);
     }
 
+    /// Build the [`Demux`].
+    ///
+    /// [`Demux`]: TODO
     pub fn build(self) -> Demux<Upd, Err> {
         Demux { handlers: self.handlers.into() }
     }
