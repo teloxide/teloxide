d25f1a4f8b4744c90789229b0d56200aa1a8a1bf
p0lunin <dmytro.polunin@gmail.com>
2021-01-29 12:31:01 +0200

    docs for dispatch_error.rs module

--- src/dispatching/core/dispatch_error.rs
+++ src/dispatching/core/dispatch_error.rs
@@ -1,5 +1,8 @@
 use std::convert::Infallible;
 
+/// The result of the handling by the user function.
+///
+/// We use our enum isntead of `std::result::Result` due to restrictions of the orphan rules.
 pub enum HandleResult<Err> {
     Ok,
     Err(Err),
@@ -20,8 +23,16 @@ impl From<()> for HandleResult<Infallible> {
     }
 }
 
+/// The result of the handling by the internal handlers or dispatchers.
+///
+/// It must be returned from the [`Handler`] (see [`HandleFuture`] type).
+///
+/// [`Handler`]: TODO
+/// [`HandleFuture`]: TODO
 #[derive(Debug, PartialEq)]
 pub enum DispatchError<Upd, Err> {
+    /// There are no handler to handle the incoming update.
     NoHandler(Upd),
+    /// There are handler which try to handle the update but it return the error.
     HandlerError(Err),
 }
