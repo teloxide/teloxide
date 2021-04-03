44956a4f858396154d6058535ee5a8d7c3baccd3
Temirkhan Myrzamadi <hirrolot@gmail.com>
2021-04-03 13:28:26 +0600

    Add descriptions of #[must_use] in Storage

--- src/dispatching/dialogue/storage/mod.rs
+++ src/dispatching/dialogue/storage/mod.rs
@@ -43,7 +43,7 @@ pub trait Storage<D> {
     type Error;
 
     /// Removes a dialogue indexed by `chat_id`.
-    #[must_use]
+    #[must_use = "You must .await it as futures are lazy"]
     fn remove_dialogue(
         self: Arc<Self>,
         chat_id: i64,
@@ -52,7 +52,7 @@ pub trait Storage<D> {
         D: Send + 'static;
 
     /// Updates a dialogue indexed by `chat_id` with `dialogue`.
-    #[must_use]
+    #[must_use = "You must .await it as futures are lazy"]
     fn update_dialogue(
         self: Arc<Self>,
         chat_id: i64,
@@ -62,7 +62,7 @@ pub trait Storage<D> {
         D: Send + 'static;
 
     /// Provides a dialogue indexed by `chat_id`.
-    #[must_use]
+    #[must_use = "You must .await it as futures are lazy"]
     fn get_dialogue(
         self: Arc<Self>,
         chat_id: i64,
