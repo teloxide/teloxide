5d0713e742bf7b1ad1d904bbb791fe1cd7104ffe
Waffle <waffle.lapkin@gmail.com>
2021-04-22 17:29:27 +0300

    Fix return types of methods
    Fix return types of LeaveChat, PinChatMessage, SetChatDescription, SetChatPhoto,
    SetChatTitle, UnpinAllChatMessages and UnpinChatMessage: String => True

--- src/payloads/set_chat_photo.rs
+++ src/payloads/set_chat_photo.rs
@@ -8,13 +8,13 @@
 // [`schema`]: https://github.com/WaffleLapkin/tg-methods-schema
 use serde::Serialize;
 
-use crate::types::{ChatId, InputFile};
+use crate::types::{ChatId, InputFile, True};
 
 impl_payload! {
     @[multipart]
     /// Use this method to set a new profile photo for the chat. Photos can't be changed for private chats. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. Returns _True_ on success.
     #[derive(Debug, PartialEq, Eq, Hash, Clone, Serialize)]
-    pub SetChatPhoto (SetChatPhotoSetters) => String {
+    pub SetChatPhoto (SetChatPhotoSetters) => True {
         required {
             /// Unique identifier for the target chat or username of the target channel (in the format `@channelusername`)
             pub chat_id: ChatId [into],
--- src/payloads/unpin_chat_message.rs
+++ src/payloads/unpin_chat_message.rs
@@ -8,12 +8,12 @@
 // [`schema`]: https://github.com/WaffleLapkin/tg-methods-schema
 use serde::Serialize;
 
-use crate::types::ChatId;
+use crate::types::{ChatId, True};
 
 impl_payload! {
     /// Use this method to remove a message from the list of pinned messages in a chat. If the chat is not a private chat, the bot must be an administrator in the chat for this to work and must have the 'can_pin_messages' admin right in a supergroup or 'can_edit_messages' admin right in a channel. Returns _True_ on success.
     #[derive(Debug, PartialEq, Eq, Hash, Clone, Serialize)]
-    pub UnpinChatMessage (UnpinChatMessageSetters) => String {
+    pub UnpinChatMessage (UnpinChatMessageSetters) => True {
         required {
             /// Unique identifier for the target chat or username of the target channel (in the format `@channelusername`)
             pub chat_id: ChatId [into],
