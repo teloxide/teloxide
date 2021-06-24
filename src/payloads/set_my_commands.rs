656e393779ce62b4c4eeb872eaaeab5a2a9b3148
Waffle <waffle.lapkin@gmail.com>
2021-06-24 20:49:26 +0300

    Fix rt-tys of `{get,set}_my_commands` methods
    Fix return types of `get_my_commands` (`u32` => `True`) and `set_my commands`
    (`u32` => `Vec<BotCommand>`).

--- src/payloads/set_my_commands.rs
+++ src/payloads/set_my_commands.rs
@@ -8,12 +8,12 @@
 // [`schema`]: https://github.com/WaffleLapkin/tg-methods-schema
 use serde::Serialize;
 
-use crate::types::BotCommand;
+use crate::types::{BotCommand, True};
 
 impl_payload! {
     /// Use this method to change the list of the bot's commands. Returns _True_ on success.
     #[derive(Debug, PartialEq, Eq, Hash, Clone, Serialize)]
-    pub SetMyCommands (SetMyCommandsSetters) => u32 {
+    pub SetMyCommands (SetMyCommandsSetters) => True {
         required {
             /// A JSON-serialized list of bot commands to be set as the list of the bot's commands. At most 100 commands can be specified.
             pub commands: Vec<BotCommand> [collect],
