183486a04490d1904117ba2e16c96401a81e7abf
Waffle <waffle.lapkin@gmail.com>
2021-07-06 15:10:22 +0300

    Update heroku and ngrok examples

--- examples/heroku_ping_pong_bot/src/main.rs
+++ examples/heroku_ping_pong_bot/src/main.rs
@@ -8,7 +8,7 @@ use tokio::sync::mpsc;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use warp::Filter;
 
-use reqwest::StatusCode;
+use reqwest::{StatusCode, Url};
 
 #[tokio::main]
 async fn main() {
@@ -30,7 +30,7 @@ pub async fn webhook(bot: AutoSend<Bot>) -> impl update_listeners::UpdateListene
     // Heroku host example .: "heroku-ping-pong-bot.herokuapp.com"
     let host = env::var("HOST").expect("have HOST env variable");
     let path = format!("bot{}", teloxide_token);
-    let url = format!("https://{}/{}", host, path);
+    let url = Url::parse(&format!("https://{}/{}", host, path)).unwrap();
 
     bot.set_webhook(url).await.expect("Cannot setup a webhook");
 
