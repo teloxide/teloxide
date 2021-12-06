a5a1277636045b018879588bd397a342b7361579
Sprite <SpriteOvO@gmail.com>
2021-12-07 03:39:02 +0800

    Fix the incorrect response type of `CopyMessage` method

--- src/types/message_id.rs
+++ src/types/message_id.rs
@@ -1,4 +1,7 @@
+use serde::{Deserialize, Serialize};
+
 /// This object represents a unique message identifier.
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 pub struct MessageId {
     /// Unique message identifier
     pub message_id: i32,
