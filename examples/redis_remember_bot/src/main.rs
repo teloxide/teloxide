980aab5fd017d2c662db5be3936ddbc37d547d6b
p0lunin <dmytro.polunin@gmail.com>
2022-01-06 13:54:07 +0200

    update redis_remember_bot example

--- examples/redis_remember_bot/src/main.rs
+++ examples/redis_remember_bot/src/main.rs
@@ -1,19 +1,14 @@
-#[macro_use]
-extern crate derive_more;
-
-mod states;
-mod transitions;
-
-use states::*;
-
 use teloxide::{
-    dispatching::dialogue::{serializer::Bincode, RedisStorage, Storage},
+    dispatching2::dialogue::{serializer::Bincode, RedisStorage, Storage},
     prelude::*,
     RequestError,
 };
 use thiserror::Error;
 
-type StorageError = <RedisStorage<Bincode> as Storage<Dialogue>>::Error;
+type Store = RedisStorage<Bincode>;
+// FIXME: naming
+type MyDialogue = Dialogue<BotDialogue, Store>;
+type StorageError = <RedisStorage<Bincode> as Storage<BotDialogue>>::Error;
 
 #[derive(Debug, Error)]
 enum Error {
@@ -23,36 +18,73 @@ enum Error {
     StorageError(#[from] StorageError),
 }
 
-type In = DialogueWithCx<AutoSend<Bot>, Message, Dialogue, StorageError>;
+#[derive(serde::Serialize, serde::Deserialize)]
+pub enum BotDialogue {
+    Start,
+    HaveNumber(i32),
+}
+
+impl Default for BotDialogue {
+    fn default() -> Self {
+        Self::Start
+    }
+}
+
+async fn handle_message(
+    bot: AutoSend<Bot>,
+    mes: Message,
+    dialogue: MyDialogue,
+) -> Result<(), Error> {
+    match mes.text() {
+        None => {
+            bot.send_message(mes.chat.id, "Send me a text message.").await?;
+        }
+        Some(ans) => {
+            let state = dialogue.current_state_or_default().await?;
+            match state {
+                BotDialogue::Start => {
+                    if let Ok(number) = ans.parse() {
+                        dialogue.next(BotDialogue::HaveNumber(number)).await?;
+                        bot.send_message(
+                            mes.chat.id,
+                            format!("Remembered number {}. Now use /get or /reset", number),
+                        )
+                        .await?;
+                    } else {
+                        bot.send_message(mes.chat.id, "Please, send me a number").await?;
+                    }
+                }
+                BotDialogue::HaveNumber(num) => {
+                    if ans.starts_with("/get") {
+                        bot.send_message(mes.chat.id, format!("Here is your number: {}", num))
+                            .await?;
+                    } else if ans.starts_with("/reset") {
+                        dialogue.reset().await?;
+                        bot.send_message(mes.chat.id, "Resetted number").await?;
+                    } else {
+                        bot.send_message(mes.chat.id, "Please, send /get or /reset").await?;
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
     let bot = Bot::from_env().auto_send();
+    // You can also choose serializer::JSON or serializer::CBOR
+    // All serializers but JSON require enabling feature
+    // "serializer-<name>", e. g. "serializer-cbor"
+    // or "serializer-bincode"
+    let storage = RedisStorage::open("redis://127.0.0.1:6379", Bincode).await.unwrap();
+
     Dispatcher::new(bot)
-        .messages_handler(DialogueDispatcher::with_storage(
-            |DialogueWithCx { cx, dialogue }: In| async move {
-                let dialogue = dialogue.expect("std::convert::Infallible");
-                handle_message(cx, dialogue).await.expect("Something wrong with the bot!")
-            },
-            // You can also choose serializer::JSON or serializer::CBOR
-            // All serializers but JSON require enabling feature
-            // "serializer-<name>", e. g. "serializer-cbor"
-            // or "serializer-bincode"
-            RedisStorage::open("redis://127.0.0.1:6379", Bincode).await.unwrap(),
-        ))
+        .dependencies(dptree::deps![storage])
+        .messages_handler(|h| {
+            h.add_dialogue::<Message, Store, BotDialogue>().branch(dptree::endpoint(handle_message))
+        })
         .dispatch()
         .await;
-}
-
-async fn handle_message(
-    cx: UpdateWithCx<AutoSend<Bot>, Message>,
-    dialogue: Dialogue,
-) -> TransitionOut<Dialogue> {
-    match cx.update.text().map(ToOwned::to_owned) {
-        None => {
-            cx.answer("Send me a text message.").await?;
-            next(dialogue)
-        }
-        Some(ans) => dialogue.react(cx, ans).await,
-    }
 }
