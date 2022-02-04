42e1cd2cd156bbafe1987e2d044ee2be51104e8b
Hirrolot <hirrolot@gmail.com>
2022-02-05 02:43:12 +0600

    Update an example in `src/dispatching2/mod.rs`

--- src/dispatching2/mod.rs
+++ src/dispatching2/mod.rs
@@ -31,12 +31,10 @@
 //! ```no_run
 //! use std::sync::atomic::{AtomicU64, Ordering};
 //!
-//! use lazy_static::lazy_static;
+//! use once_cell::sync::Lazy;
 //! use teloxide::prelude2::*;
 //!
-//! lazy_static! {
-//!     static ref MESSAGES_TOTAL: AtomicU64 = AtomicU64::new(0);
-//! }
+//! static MESSAGES_TOTAL: Lazy<AtomicU64> = Lazy::new(AtomicU64::default);
 //!
 //! # #[tokio::main]
 //! # async fn main() {
