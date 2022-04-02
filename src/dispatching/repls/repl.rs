2f5ce528df1d6d9725e5ae11a0f5582719aa4a5b
Hirrolot <hirrolot@gmail.com>
2022-04-02 21:03:54 +0600

    Fix the `repl_is_send` test

--- src/dispatching/repls/repl.rs
+++ src/dispatching/repls/repl.rs
@@ -73,7 +73,7 @@ where
 #[test]
 fn repl_is_send() {
     let bot = crate::Bot::new("");
-    let repl = crate::repl(bot, |_| async { crate::respond(()) });
+    let repl = crate::repl(bot, || async { crate::respond(()) });
     assert_send(&repl);
 
     fn assert_send(_: &impl Send) {}
