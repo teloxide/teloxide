sorz/teloxide-core

make `errors` module pub and add `errors::AsResponseParameters` trait

src/errors.rs
@@ -5,17 +5,7 @@ use reqwest::StatusCode;
 use serde::Deserialize;
 use thiserror::Error;
 
-/// An error caused by downloading a file.
-#[derive(Debug, Error, From)]
-pub enum DownloadError {
-    /// A network error while downloading a file from Telegram.
-    #[error("A network error: {0}")]
-    NetworkError(#[source] reqwest::Error),
-
-    /// An I/O error while writing a file to destination.
-    #[error("An I/O error: {0}")]
-    Io(#[source] std::io::Error),
-}
+use crate::types::ResponseParameters;
 
 /// An error caused by sending a request to Telegram.
 #[derive(Debug, Error)]
@@ -56,6 +46,46 @@ pub enum RequestError {
     /// Occurs when trying to send a file to Telegram.
     #[error("An I/O error: {0}")]
     Io(#[source] io::Error),
+}
+
+pub trait AsResponseParameters {
+    fn response_parameters(&self) -> Option<ResponseParameters>;
+
+    fn retry_after(&self) -> Option<u32> {
+        self.response_parameters().and_then(|rp| match rp {
+            ResponseParameters::RetryAfter(n) => Some(n),
+            _ => None,
+        })
+    }
+
+    fn migrate_to_chat_id(&self) -> Option<i64> {
+        self.response_parameters().and_then(|rp| match rp {
+            ResponseParameters::MigrateToChatId(id) => Some(id),
+            _ => None,
+        })
+    }
+}
+
+impl AsResponseParameters for crate::RequestError {
+    fn response_parameters(&self) -> Option<ResponseParameters> {
+        match self {
+            &Self::RetryAfter(n) => Some(ResponseParameters::RetryAfter(n)),
+            &Self::MigrateToChatId(id) => Some(ResponseParameters::MigrateToChatId(id)),
+            _ => None,
+        }
+    }
+}
+
+/// An error caused by downloading a file.
+#[derive(Debug, Error, From)]
+pub enum DownloadError {
+    /// A network error while downloading a file from Telegram.
+    #[error("A network error: {0}")]
+    NetworkError(#[source] reqwest::Error),
+
+    /// An I/O error while writing a file to destination.
+    #[error("An I/O error: {0}")]
+    Io(#[source] std::io::Error),
 }
 
 /// A kind of an API error.

teloxide/teloxide

src/errors.rs
@@ -3,18 +3,7 @@ use std::io;
 use serde::Deserialize;
 use thiserror::Error;
 
-/// An error caused by downloading a file.
-#[derive(Debug, Error)]
-pub enum DownloadError {
-    /// A network error while downloading a file from Telegram.
-    #[error("A network error: {0}")]
-    // NOTE: this variant must not be created by anything except the From impl
-    Network(#[source] reqwest::Error),
-
-    /// An I/O error while writing a file to destination.
-    #[error("An I/O error: {0}")]
-    Io(#[from] std::io::Error),
-}
+use crate::types::ResponseParameters;
 
 /// An error caused by sending a request to Telegram.
 #[derive(Debug, Error)]
@@ -55,6 +44,47 @@ pub enum RequestError {
     /// Occurs when trying to send a file to Telegram.
     #[error("An I/O error: {0}")]
     Io(#[source] io::Error),
+}
+
+/// An error caused by downloading a file.
+#[derive(Debug, Error)]
+pub enum DownloadError {
+    /// A network error while downloading a file from Telegram.
+    #[error("A network error: {0}")]
+    // NOTE: this variant must not be created by anything except the From impl
+    Network(#[source] reqwest::Error),
+
+    /// An I/O error while writing a file to destination.
+    #[error("An I/O error: {0}")]
+    Io(#[from] std::io::Error),
+}
+
+pub trait AsResponseParameters {
+    fn response_parameters(&self) -> Option<ResponseParameters>;
+
+    fn retry_after(&self) -> Option<u32> {
+        self.response_parameters().and_then(|rp| match rp {
+            ResponseParameters::RetryAfter(n) => Some(n),
+            _ => None,
+        })
+    }
+
+    fn migrate_to_chat_id(&self) -> Option<i64> {
+        self.response_parameters().and_then(|rp| match rp {
+            ResponseParameters::MigrateToChatId(id) => Some(id),
+            _ => None,
+        })
+    }
+}
+
+impl AsResponseParameters for crate::RequestError {
+    fn response_parameters(&self) -> Option<ResponseParameters> {
+        match self {
+            &Self::RetryAfter(n) => Some(ResponseParameters::RetryAfter(n)),
+            &Self::MigrateToChatId(id) => Some(ResponseParameters::MigrateToChatId(id)),
+            _ => None,
+        }
+    }
 }
 
 /// A kind of an API error.