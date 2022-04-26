bbb3ac6e2d4c80f40ffb77b7fbd18646b6ca3ece
Hirrolot <hirrolot@gmail.com>
2022-04-27 00:54:37 +0600

    `examples/simple_commands.rs`=> `examples/command.rs`
    Former-commit-id: 5ecfd3d810da281d8deb88860382e093d300d398

--- examples/command.rs
+++ examples/command.rs
@@ -5,7 +5,7 @@ use std::error::Error;
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting simple_commands_bot...");
+    log::info!("Starting command bot...");
 
     let bot = Bot::from_env().auto_send();
 
