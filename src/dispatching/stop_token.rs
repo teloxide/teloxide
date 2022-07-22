611a38990e0cdacabeb5a506652393d3d6b7708f
Maybe Waffle <waffle.lapkin@gmail.com>
2022-07-22 15:52:25 +0400

    fix typos
    Former-commit-id: fd12e1eb06cdc7ff531cb10dde04ca67fad83267

--- src/dispatching/stop_token.rs
+++ src/dispatching/stop_token.rs
@@ -39,7 +39,7 @@ pub struct AsyncStopFlag(#[pin] Abortable<Pending<()>>);
 
 impl AsyncStopToken {
     /// Create a new token/flag pair.
-    #[must_use = "This function is pure, that is does nothing unless it's output is used"]
+    #[must_use = "This function is pure, that is does nothing unless its output is used"]
     pub fn new_pair() -> (Self, AsyncStopFlag) {
         let (handle, reg) = AbortHandle::new_pair();
         let token = Self(handle);
@@ -57,7 +57,7 @@ impl StopToken for AsyncStopToken {
 
 impl AsyncStopFlag {
     /// Returns true if the stop token linked to `self` was used.
-    #[must_use = "This function is pure, that is does nothing unless it's output is used"]
+    #[must_use = "This function is pure, that is does nothing unless its output is used"]
     pub fn is_stopped(&self) -> bool {
         self.0.is_aborted()
     }
