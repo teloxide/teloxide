af5f27feee85467ed770122163d6baf706110069
Maybe Waffle <waffle.lapkin@gmail.com>
2022-10-07 14:11:56 +0400

    link "advanced features" in `repls` to `dispatching`
    Former-commit-id: 583e5ea4abeeb94456b1e1bb04c192d19ad89d8d

--- src/dispatching/repls.rs
+++ src/dispatching/repls.rs
@@ -3,9 +3,10 @@
 //! This module provides utilities for easy update handling. They accept a
 //! single "handler" function that processes all updates of a certain kind. Note
 //! that REPLs are meant to be used for simple scenarios, such as prototyping,
-//! inasmuch they lack configuration and some advanced features.
+//! inasmuch they lack configuration and some [advanced features].
 //!
 //! [REPL]: https://en.wikipedia.org/wiki/Read-eval-print_loop
+//! [advanced features]: crate::dispatching#dispatching-or-repls
 
 mod commands_repl;
 mod repl;
