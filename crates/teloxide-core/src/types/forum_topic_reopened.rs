f5afbf2188a6d3591da326259ee9d32d9b71b80b
Maybe Waffle <waffle.lapkin@gmail.com>
2023-01-14 18:23:13 +0400

    doc fix

--- crates/teloxide-core/src/types/forum_topic_reopened.rs
+++ crates/teloxide-core/src/types/forum_topic_reopened.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 /// This object represents a service message about a forum topic reopened in the
 /// chat. Currently holds no information.
 ///
-/// [The official docs](https://core.telegram.org/bots/api#forumtopiccreated).
+/// [The official docs](https://core.telegram.org/bots/api#forumtopicreopened).
 #[serde_with_macros::skip_serializing_none]
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
 pub struct ForumTopicReopened;
