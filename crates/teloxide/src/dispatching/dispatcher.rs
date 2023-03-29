15ada104776f5f9f2e4802486a62c27380329585
Aliaksei Saladukhin <studiedlist@gmail.com>
2023-03-29 12:56:23 +0200

    add fast exit with double ctrlc

--- crates/teloxide/src/dispatching/dispatcher.rs
+++ crates/teloxide/src/dispatching/dispatcher.rs
@@ -284,7 +284,15 @@ where
         let error_handler =
             LoggingErrorHandler::with_custom_text("An error from the update listener");
 
-        self.dispatch_with_listener(listener, error_handler).await;
+        let ctrlc = async {
+            tokio::signal::ctrl_c().await.expect("Failed to listen for ^C");
+            tokio::signal::ctrl_c().await.expect("Failed to listen for ^C");
+        };
+
+        tokio::select!(
+            _ = self.dispatch_with_listener(listener, error_handler) => (),
+            _ = ctrlc => (),
+        );
     }
 
     /// Starts your bot with custom `update_listener` and
