8d858c43ff2ac27ca34a7e6063750b997d39a432
jrx <jerome+git@rouaix.eu>
2023-08-31 23:03:54 +0200

    revert tryable repls

--- crates/teloxide/src/repls/repl.rs
+++ crates/teloxide/src/repls/repl.rs
@@ -55,60 +55,8 @@ where
     <R as Requester>::GetUpdates: Send,
     H: Injectable<DependencyMap, ResponseResult<()>, Args> + Send + Sync + 'static,
 {
-    try_repl(bot, handler).await.expect("try_repl failed")
-}
-
-/// Same as `repl` but returns a Result<_> instead of panicking
-///
-/// A [REPL] for messages.
-//
-///
-//
-#[doc = include_str!("preamble.md")]
-///
-/// [REPL]: https://en.wikipedia.org/wiki/Read-eval-print_loop
-///
-/// ## Signature
-///
-/// Don't be scared by many trait bounds in the signature, in essence they
-/// require:
-///
-/// 1. `bot` is a bot, client for the Telegram bot API. It is represented via
-///    the [`Requester`] trait.
-/// 2. `handler` is an `async` function that takes arguments from
-///    [`DependencyMap`] (see below) and returns [`ResponseResult`].
-///
-/// ## Handler arguments
-///
-/// `teloxide` provides the following types to the `handler`:
-/// - [`Message`]
-/// - `R` (type of the `bot`)
-/// - [`Me`]
-///
-/// Each of these types can be accepted as a handler parameter. Note that they
-/// aren't all required at the same time: e.g., you can take only the bot and
-/// the message without [`Me`].
-///
-/// [`Me`]: crate::types::Me
-/// [`Message`]: crate::types::Message
-///
-/// ## Stopping
-//
-#[doc = include_str!("stopping.md")]
-///
-/// ## Caution
-//
-#[doc = include_str!("caution.md")]
-///
-#[cfg(feature = "ctrlc_handler")]
-pub async fn try_repl<R, H, Args>(bot: R, handler: H) -> Result<(), R::Err>
-where
-    R: Requester + Send + Sync + Clone + 'static,
-    <R as Requester>::GetUpdates: Send,
-    H: Injectable<DependencyMap, ResponseResult<()>, Args> + Send + Sync + 'static,
-{
     let cloned_bot = bot.clone();
-    try_repl_with_listener(bot, handler, update_listeners::polling_default(cloned_bot).await).await
+    repl_with_listener(bot, handler, update_listeners::polling_default(cloned_bot).await).await;
 }
 
 /// A [REPL] for messages, with a custom [`UpdateListener`].
@@ -162,66 +110,6 @@ where
     L: UpdateListener + Send,
     L::Err: Debug,
 {
-    try_repl_with_listener(bot, handler, listener).await.expect("try_repl_with_listener failed")
-}
-
-/// Same as `repl_with_listener` but returns a Result<_> instead of panicking
-///
-/// A [REPL] for messages, with a custom [`UpdateListener`].
-//
-///
-//
-#[doc = include_str!("preamble.md")]
-///
-/// [REPL]: https://en.wikipedia.org/wiki/Read-eval-print_loop
-/// [`UpdateListener`]: crate::update_listeners::UpdateListener
-///
-/// ## Signature
-///
-/// Don't be scared by many trait bounds in the signature, in essence they
-/// require:
-///
-/// 1. `bot` is a bot, client for the Telegram bot API. It is represented via
-///    the [`Requester`] trait.
-/// 2. `handler` is an `async` function that takes arguments from
-///    [`DependencyMap`] (see below) and returns [`ResponseResult`].
-/// 3. `listener` is something that takes updates from a Telegram server and
-///    implements [`UpdateListener`].
-///
-/// ## Handler arguments
-///
-/// `teloxide` provides the following types to the `handler`:
-/// - [`Message`]
-/// - `R` (type of the `bot`)
-/// - [`Me`]
-///
-/// Each of these types can be accepted as a handler parameter. Note that they
-/// aren't all required at the same time: e.g., you can take only the bot and
-/// the message without [`Me`].
-///
-/// [`Me`]: crate::types::Me
-/// [`Message`]: crate::types::Message
-///
-/// ## Stopping
-//
-#[doc = include_str!("stopping.md")]
-///
-/// ## Caution
-//
-#[doc = include_str!("caution.md")]
-///
-#[cfg(feature = "ctrlc_handler")]
-pub async fn try_repl_with_listener<R, H, L, Args>(
-    bot: R,
-    handler: H,
-    listener: L,
-) -> Result<(), R::Err>
-where
-    R: Requester + Clone + Send + Sync + 'static,
-    H: Injectable<DependencyMap, ResponseResult<()>, Args> + Send + Sync + 'static,
-    L: UpdateListener + Send,
-    L::Err: Debug,
-{
     use crate::dispatching::Dispatcher;
 
     // Other update types are of no interest to use since this REPL is only for
@@ -232,13 +120,11 @@ where
         .default_handler(ignore_update)
         .enable_ctrlc_handler()
         .build()
-        .try_dispatch_with_listener(
+        .dispatch_with_listener(
             listener,
             LoggingErrorHandler::with_custom_text("An error from the update listener"),
         )
-        .await?;
-
-    Ok(())
+        .await;
 }
 
 #[test]
