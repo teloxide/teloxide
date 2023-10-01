9fc2bac03874d16f7c494bf4e8ae72436ac87f94
aon <21188659+aon@users.noreply.github.com>
2023-10-01 12:29:40 -0300

    Add `Injectable` to `with_tracing_span`

--- crates/teloxide/src/dispatching/tracing.rs
+++ crates/teloxide/src/dispatching/tracing.rs
@@ -1,29 +1,34 @@
 use super::{DpHandlerDescription, UpdateHandler};
 
-use dptree::{prelude::DependencyMap, HandlerDescription};
-use tracing::Instrument;
+use dptree::{di::Injectable, prelude::DependencyMap, HandlerDescription};
+use tracing::{Instrument, Span};
 
 /// Extension methods for adding `tracing` to [`UpdateHandler`].
 ///
 /// This trait is only available if the `tracing` feature is enabled.
 pub trait UpdateHandlerTracingExt<E> {
     /// Returns an `UpdateHandler` with tracing enabled.
-    fn with_tracing_span<F>(self, f: F) -> Self
+    fn with_tracing_span<F, FnArgs>(self, f: F) -> Self
     where
-        F: Fn(DependencyMap) -> tracing::Span + Send + Sync + 'static;
+        F: Injectable<DependencyMap, Span, FnArgs> + Send + Sync + Clone + 'static;
 }
 
 impl<E: 'static> UpdateHandlerTracingExt<E> for UpdateHandler<E> {
-    fn with_tracing_span<F>(self, f: F) -> Self
+    fn with_tracing_span<F, FnArgs>(self, f: F) -> UpdateHandler<E>
     where
-        F: Fn(DependencyMap) -> tracing::Span + Send + Sync + 'static,
+        F: Injectable<DependencyMap, Span, FnArgs> + Send + Sync + Clone + 'static,
     {
         // FIXME: This is a hacky replacement for `handler.description().clone()`.
         // Ideally cloning `DpHandlerDescription` would be supported by `teloxide`.
         let description = DpHandlerDescription::entry().merge_chain(self.description());
 
         dptree::from_fn_with_description(description, move |deps: DependencyMap, cont| {
-            self.clone().execute(deps.clone(), cont).instrument(f(deps))
+            let self_c = self.clone();
+            let f_c = f.clone();
+            async move {
+                let span = f_c.inject(&deps)().await;
+                self_c.execute(deps, cont).instrument(span).await
+            }
         })
     }
 }
