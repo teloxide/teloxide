3e05086d47a50bbbaf52860edda3ff496473471f
TheAwiteb <awiteb@hotmail.com>
2023-10-05 00:32:47 +0300

    Add the `use_independent_chat_permissions` field to `RestrictChatMember` and `SetChatPermissions`

--- crates/teloxide-core/src/payloads/restrict_chat_member.rs
+++ crates/teloxide-core/src/payloads/restrict_chat_member.rs
@@ -18,6 +18,8 @@ impl_payload! {
             pub permissions: ChatPermissions,
         }
         optional {
+            /// Pass _True_ if chat permissions are set independently. Otherwise, the _can\_send\_other\_messages_ and _can\_add\_web\_page\_previews_ permissions will imply the _can\_send\_messages_, _can\_send\_audios_, _can\_send\_documents_, _can\_send\_photos_, _can\_send\_videos_, _can\_send\_video\_notes_, and _can\_send\_voice\_notes_ permissions; the _can\_send\_polls_ permission will imply the _can\_send\_messages_ permission.
+            pub use_independent_chat_permissions: bool,
             /// Date when the user will be unbanned, unix time. If user is banned for more than 366 days or less than 30 seconds from the current time they are considered to be banned forever
             #[serde(with = "crate::types::serde_opt_date_from_unix_timestamp")]
             pub until_date: DateTime<Utc> [into],
