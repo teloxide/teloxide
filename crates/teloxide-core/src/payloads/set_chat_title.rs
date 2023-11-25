bacd84110e69fa591ae6a70e7b2de1007415ed89
Desiders <abc_desi@proton.me>
2023-11-25 19:21:21 +0300

    Change setChatTitle title length

--- crates/teloxide-core/src/payloads/set_chat_title.rs
+++ crates/teloxide-core/src/payloads/set_chat_title.rs
@@ -11,7 +11,7 @@ impl_payload! {
         required {
             /// Unique identifier for the target chat or username of the target channel (in the format `@channelusername`)
             pub chat_id: Recipient [into],
-            /// New chat title, 1-255 characters
+            /// New chat title, 1-128 characters
             pub title: String [into],
         }
     }
