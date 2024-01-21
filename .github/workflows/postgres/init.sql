3fb4ee226acabeb36a689bdd4583b9839efbdc34
Сырцев Вадим Игоревич <syrtcevvi@gmail.com>
2024-01-21 12:08:28 +0300

    Update ci.yml to allow Postgres testing

---
+++ .github/workflows/postgres/init.sql
@@ -0,0 +1,4 @@
+CREATE ROLE teloxide WITH LOGIN SUPERUSER PASSWORD 'rewrite_it_in_rust'
+CREATE DATABASE test_postgres_json;
+CREATE DATABASE test_postgres_bincode;
+CREATE_DATABASE test_postgres_cbor;
