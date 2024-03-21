cedf223b0dbfcad0f04a2dfb107e8b49c552a71e
Maybe Waffle <waffle.lapkin@gmail.com>
2024-03-21 12:24:07 +0100

    update nightly

--- crates/teloxide-core/src/codegen.rs
+++ crates/teloxide-core/src/codegen.rs
@@ -23,7 +23,7 @@ use xshell::{cmd, Shell};
 
 fn ensure_rustfmt(sh: &Shell) {
     // FIXME(waffle): find a better way to set toolchain
-    let toolchain = "nightly-2023-09-27";
+    let toolchain = "nightly-2024-03-20";
 
     let version = cmd!(sh, "rustup run {toolchain} rustfmt --version").read().unwrap_or_default();
 
@@ -36,7 +36,7 @@ fn ensure_rustfmt(sh: &Shell) {
 }
 
 pub fn reformat(text: String) -> String {
-    let toolchain = "nightly-2023-09-27";
+    let toolchain = "nightly-2024-03-20";
 
     let sh = Shell::new().unwrap();
     ensure_rustfmt(&sh);
