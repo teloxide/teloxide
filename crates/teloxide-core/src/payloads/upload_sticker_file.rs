c24fdf173e337a4927ea1bad73d845df4fc5ab1f
Сырцев Вадим Игоревич <syrtcevvi@gmail.com>
2024-06-06 18:00:18 +0300

    Fix docs for uploadStickerFile method

--- crates/teloxide-core/src/payloads/upload_sticker_file.rs
+++ crates/teloxide-core/src/payloads/upload_sticker_file.rs
@@ -6,15 +6,20 @@ use crate::types::{FileMeta, InputFile, StickerFormat, UserId};
 
 impl_payload! {
     @[multipart = sticker]
-    /// Use this method to upload a .PNG file with a sticker for later use in _createNewStickerSet_ and _addStickerToSet_ methods (can be used multiple times). Returns the uploaded File on success.
+    /// Use this method to upload a file with a sticker for later use in the [CreateNewStickerSet] and [AddStickerToSet] methods (the file can be used multiple times). Returns the uploaded [`File`] on success.
+    ///
+    /// [CreateNewStickerSet]: https://docs.rs/teloxide/latest/teloxide/payloads/struct.CreateNewStickerSet.html
+    /// [AddStickerToSet]: https://docs.rs/teloxide/latest/teloxide/payloads/struct.AddStickerToSet.html
+    /// [`File`]: crate::types::File
     #[derive(Debug, Clone, Serialize)]
     pub UploadStickerFile (UploadStickerFileSetters) => FileMeta {
         required {
             /// User identifier of sticker file owner
             pub user_id: UserId,
-            /// A file with the sticker in .WEBP, .PNG, .TGS, or .WEBM format. See https://core.telegram.org/stickers for technical requirements. [More info on Sending Files »]
+            /// A file with the sticker in .WEBP, .PNG, .TGS, or .WEBM format. [Technical requirements]. [More info on Sending Files »]
             ///
             /// [More info on Sending Files »]: crate::types::InputFile
+            /// [Technical requirements]: https://core.telegram.org/stickers
             pub sticker: InputFile,
             /// Format of the sticker, must be one of “static”, “animated”, “video”
             pub sticker_format: StickerFormat,
