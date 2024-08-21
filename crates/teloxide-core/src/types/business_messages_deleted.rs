f6c957182fc4837b5f67a132ffe8d5520b31b647
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-08-21 10:16:07 +0530

    Add `BusinessMessagesDeleted` struct

---
+++ crates/teloxide-core/src/types/business_messages_deleted.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+use crate::types::{Chat, MessageId};
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct BusinessMessagesDeleted {
+    /// Unique identifier of the business connection.
+    pub business_connection_id: String,
+
+    /// Information about a chat in the business account. The bot may not have
+    /// access to the chat or the corresponding user.
+    pub chat: Chat,
+
+    /// The list of identifiers of deleted messages in the chat of the business
+    /// account.
+    pub message_ids: Vec<MessageId>,
+}
