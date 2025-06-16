f8416860d36a5c1f243dbbba8b1c468308074b36
LasterAlex <sasha.ishukshin@gmail.com>
2025-06-17 00:01:20 +0300

    Added paid_media_purchased to Update

---
+++ crates/teloxide-core/src/types/paid_media_purchased.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+use crate::types::User;
+
+/// Describes the information about a paid media purchase.
+///
+/// [The official docs](https://core.telegram.org/bots/api#paidmediapurchased).
+#[derive(Clone, Debug)]
+#[derive(PartialEq, Eq, Hash)]
+#[derive(Serialize, Deserialize)]
+pub struct PaidMediaPurchased {
+    /// User who purchased the media
+    pub from: User,
+
+    /// Bot-specified paid media payload
+    pub paid_media_payload: String,
+}
