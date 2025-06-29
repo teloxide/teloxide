7bf08e8b2db203cb3b5183b4fb7dcaa7d3b3b73d
LasterAlex <sasha.ishukshin@gmail.com>
2025-06-30 02:18:03 +0300

    New fields + parameters + deleted hide_url

--- crates/teloxide-core/src/types/gift.rs
+++ crates/teloxide-core/src/types/gift.rs
@@ -39,6 +39,10 @@ pub struct Gift {
     /// The number of Telegram Stars that must be paid to send the sticker
     pub star_count: u32,
 
+    /// The number of Telegram Stars that must be paid to upgrade the gift to a
+    /// unique one
+    pub upgrade_star_count: Option<u32>,
+
     /// The total number of the gifts of this type that can be sent; for limited
     /// gifts only
     pub total_count: Option<u32>,
